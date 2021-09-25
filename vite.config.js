e1d6fa508afd339fc0142ce90595c751d15ef644
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-09-25 13:06:18 +0200

    add import alias

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,34 @@
+/** EXTERNALS **/
+
+
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+const path = require('path');
+
+
+/** LOCALS **/
+
+
+/** HELPERS **/
+
+const alias = [
+    {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+    },
+];
+
+
+/** MAIN **/
+
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  build: {
-    target: "esnext",
-    polyfillDynamicImport: false,
-  },
+    plugins: [solidPlugin()],
+    build: {
+        target: "esnext",
+        polyfillDynamicImport: false,
+    },
+    resolve:{
+        alias,
+    },
 });

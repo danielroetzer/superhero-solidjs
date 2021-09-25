2660db019546f4b3952ba1e26215d0712271d537
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-09-25 23:03:17 +0200

    implement TierlistPage

---
+++ src/pages/Tierlist/index.jsx
@@ -0,0 +1,48 @@
+/** EXTERNALS **/
+
+
+import { createMemo, createResource } from 'solid-js';
+import { slice } from 'rambda';
+
+
+/** LOCALS **/
+
+
+import SuperheroCard from '@/components/SuperheroCard';
+import fetchSuperheroes from '@/API/fetch/superheroes';
+
+
+/** HELPERS **/
+
+
+/** MAIN **/
+
+
+const Tierlist = function() {
+    const [superheroes] = createResource(fetchSuperheroes);
+
+    // const top10 = slice(0, 10, superheroes() ?? []);
+    const top10 = createMemo(function() {
+        return slice(0, 10, superheroes() ?? []);
+    });
+
+    return (
+        <div>
+            <For each={top10()} fallback={<div>Loading...</div>}>
+                {function(superhero) {
+                    return (
+                        <SuperheroCard
+                            name={superhero.name}
+                            img={{
+                                src: superhero.images.xs,
+                                alt: superhero.name,
+                            }}
+                        />
+                    )
+                }}
+            </For>
+        </div>
+    );
+};
+
+export default Tierlist;

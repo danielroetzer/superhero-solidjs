4189f0c10ec0624a9cdb4f676dcbd44019626eba
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-10-02 23:00:09 +0200

    eslint seup

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 8,
+        allowImportExportEverywhere: true,
+    },
+    plugins: ['solid'],
+    extends: ['eslint:recommended', 'airbnb-base', 'plugin:solid/recommended'],
+    rules: {
+        indent: ['error', 4],
+        'spaced-comment': ['error', 'always', {
+            block: {
+                exceptions: ['*'],
+                balanced: true,
+            },
+        }],
+    },
+};
